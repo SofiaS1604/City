beb6e60bb1d4899adf6fc78357e30771c4ca2c5e
SofiaS1604 <SofiaS1604@yandex.ru>
2019-03-02 17:03:15 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,34 +1,34 @@
 $(document).ready(function(){
-    $('.menu_item').click(function () {
-        $('.menu_mobile').removeClass('menu_mobile-active');
-        $('.menu').removeClass('active_menu');
-    })
-
     $("a").on("click", function(e){
         let anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $(anchor.attr('href')).offset().top
         }, 800);
         e.preventDefault();
-
     });
-
-    let active = 0;
-
-    $('.menu').click(function () {
-        if(active === 1){
-            $('.menu').removeClass('active_menu');
-            $('.menu_mobile').removeClass('menu_mobile-active');
-            active = 0;
-        }else{
-            $('.menu').addClass('active_menu');
-            $('.menu_mobile').addClass('menu_mobile-active');
-            active = 1;
-        }
-    });
-
-
-
-
-
 });
+
+document.querySelector('.menu_item').addEventListener('click',() => {
+    document.querySelector('.menu').className = 'menu';
+    document.querySelector('.menu_mobile').className = 'menu_mobile';
+});
+
+let active = 0;
+
+document.querySelector('.menu').addEventListener('click',()=>{
+    if(active === 1){
+        document.querySelector('.menu').className = 'menu';
+        document.querySelector('.menu_mobile').className = 'menu_mobile';
+        active = 0;
+    }else{
+        document.querySelector('.menu').className = 'menu active_menu';
+        document.querySelector('.menu_mobile').className = 'menu_mobile menu_mobile-active';
+        active = 1;
+    }
+});
+
+// document.querySelector('.image-1').addEventListener('click', ()=>{
+//    document.querySelector('.slider').style.display = 'flex';
+//    document.querySelector('.images').style.display = 'none';
+//    document.querySelector('.slid').innerHTML = `<img class="image image-1" src="./images/architecture-buildings-burj-khalifa-1688504.jpg" alt="">`
+// });
