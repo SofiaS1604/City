e2d0f3495f4d85dd55d8abf760860595714bb2b6
SofiaS1604 <SofiaS1604@yandex.ru>
2019-03-05 03:36:41 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
 $(document).ready(function(){
-
     $('.menu_item').click(function () {
         $('.menu_mobile').removeClass('menu_mobile-active');
         $('.menu').removeClass('active_menu');
     });
-
 
     $('a').on('click', function(e){
         let anchor = $(this);
@@ -27,4 +25,25 @@ $(document).ready(function(){
             active = 1;
         }
     });
+
+    $('.block-right').click(function () {
+        $('.slid_image-left').animate({
+            left: 200
+        },500);
+
+        $('.slid_image-center').animate({
+            left: 955
+        },500);
+
+        $('.slid_image-left').addClass('image_left');
+        $('.slid_image-right').addClass('slid_image-left').removeClass('slid_image-right');
+
+        $('.slid_image-right').animate({
+            left: 1150
+        },600);
+
+        $('.slid_image-center').addClass('slid_image-right').removeClass('slid_image-center');
+        $('.image_left').addClass('slid_image-center').removeClass('slid_image-left').removeClass('image_left');
+    });
+
 });
